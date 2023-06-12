e3118158a2f06e5b85bf52120ebaff74862022c1
jdnichollsc <jdnichollsc@hotmail.com>
2023-06-12 18:37:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# solana-wallet-balance
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-vwskn2)
---
+++ index.js
@@ -0,0 +1,28 @@
+const web3 = require('@solana/web3.js');
+const splToken = require('@solana/spl-token');
+
+const walletAddress = '9T6LCiDaEFn76sLeVHL98AxwNjj7hfFwrh62ksJwVdge';
+const usdcMintAddress = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
+
+async function getWalletBalance() {
+  const connection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'));
+  const walletPublicKey = new web3.PublicKey(walletAddress);
+  const usdcMintPublicKey = new web3.PublicKey(usdcMintAddress);
+
+  // Fetch token account balance
+  const usdcTokenAccount = await splToken.getAssociatedTokenAddress(
+    usdcMintPublicKey,
+    walletPublicKey
+  );
+  // const accountInfo = await connection.getAccountInfo(usdcTokenAccount);
+
+  const solBalance = await connection.getBalance(walletPublicKey);
+  const tokenAmount = await connection.getTokenAccountBalance(usdcTokenAccount);
+  const balance = {
+    sol: solBalance / web3.LAMPORTS_PER_SOL,
+    usdc: tokenAmount.value.uiAmount || 0,
+  };
+  return balance;
+}
+
+console.log('USDC BALANCE: ', getWalletBalance().usdc);
